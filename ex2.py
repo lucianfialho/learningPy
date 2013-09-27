ddd6709b4905d81704f51d532336333a1fcfe4fb
Lucian Fialho <lucianfialhobp@hotmail.com>
2013-09-27 16:57:53 -0300

    Adição de promoção do para compania Tchau
    Modifique o programa da empresa Tchau para uma promoção onde a tarifa é de R$0,08 centavos quando você utiliza mais que 800 minutos

--- ex2.py
+++ ex2.py
@@ -1,3 +1,4 @@
+# VAR 
 minGastos = int(input("Numero de minutos gastos"))
 if minGastos <= 200:
 	resultado = minGastos * 0.20 
@@ -5,5 +6,6 @@ elif minGastos > 200 and minGastos <= 400:
 	resultado = minGastos * 0.18
 elif minGastos > 400:
 	resultado = minGastos * 0.15
-
+elif minGastos > 800:
+	resultado = minGastos * 0.8
 print ("O total da sua conta foi", resultado)
